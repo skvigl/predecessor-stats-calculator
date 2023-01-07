f09e67c54aafe5b58dfd7a225162158b6fcc1868
skvigl <skvigl@gmail.com>
2023-01-07 13:49:18 +0500

    Add font-awesome

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import 'tippy.js/dist/tippy.css'
 
 import './styles/normalize.css'
-import './styles/index.css'
 import './styles/Tippy.css'
+import './styles/index.css'
+import './fontawesome/css/fontawesome.css'
+import './fontawesome/css/brands.css'
+import './fontawesome/css/solid.css'
 
 import App from './App'
 
@@ -12,5 +15,5 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
