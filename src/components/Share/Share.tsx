108a3289a09a97c94f7f7f971600bda4ed5e3c9f
skvigl <skvigl@gmail.com>
2023-01-31 17:11:32 +0500

    Change scrollbar sizes

--- src/components/Share/Share.tsx
+++ src/components/Share/Share.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import _ from 'lodash'
 
 import { heroBuildsService } from '../../services/implementations'
