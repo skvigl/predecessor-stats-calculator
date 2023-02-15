f4d023a1ae685cdcd46281c546cd2fa409f220dd
skvigl <skvigl@gmail.com>
2023-02-15 20:31:47 +0500

    Update Panel styles

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -9,10 +9,13 @@ interface PanelProps extends PropsWithChildren {
 export const Panel: React.FC<PanelProps> = ({ onClose, children }) => {
   return (
     <div className='panel'>
-      <button className='panel-close' onClick={onClose}>
-        <i className='fa-solid fa-xmark'></i>
-      </button>
-      {children}
+      <div className='panel-overlay'></div>
+      <div className='panel-body'>
+        <button className='panel-close' onClick={onClose}>
+          <i className='fa-solid fa-xmark'></i>
+        </button>
+        {children}
+      </div>
     </div>
   )
 }
