31f7cb1e1e918827b8c20c0fb2b1bad0d6f7c8d4
skvigl <skvigl@gmail.com>
2023-02-15 20:31:22 +0500

    Add size prop to Thumbnail

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -6,13 +6,21 @@ import './Thumbnail.css'
 
 interface ThumbnailProps {
   name: string
+  size?: number
   forwardedRef?: React.Ref<HTMLDivElement>
   onClick?: (event: React.MouseEvent) => void
 }
 
-export const Thumbnail: React.FC<ThumbnailProps> = ({ name, forwardedRef, onClick = _.identity }) => {
+export const Thumbnail: React.FC<ThumbnailProps> = ({ name, size = 6, forwardedRef, onClick = _.identity }) => {
   return (
-    <div ref={forwardedRef} className='thumbnail' onClick={onClick} data-id={name} role='button'>
+    <div
+      ref={forwardedRef}
+      className='thumbnail'
+      style={{ width: 10 * size, height: 10 * size }}
+      onClick={onClick}
+      data-id={name}
+      role='button'
+    >
       <img className='thumbnail-image' src={getImageSrc(name)} alt={name} />
     </div>
   )
